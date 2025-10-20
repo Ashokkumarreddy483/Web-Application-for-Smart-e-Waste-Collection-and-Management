5b46b8ed4c381f301aebd056c3d8780d4c17ffdd
Ashok kumar reddy <dashokkumarreddy2002@gmail.com>
2025-10-20 18:15:31 +0530

    Remove src folder from smart-ewaste-frontend

--- smart-ewaste-frontend/src/services/auth.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:8080/api";
-
-export const register = (userData) => axios.post(`${API_URL}/auth/register`, userData);
-export const login = (loginData) => axios.post(`${API_URL}/auth/login`, loginData);
-export const getProfile = (id) => axios.get(`${API_URL}/users/profile/${id}`);
-export const updateProfile = (id, data) => axios.put(`${API_URL}/users/profile/${id}`, data);
